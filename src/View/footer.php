63d2116612424cec07724ccc3ae7532d00e4891b
LyraMaker <gabriel.lyrat@gmail.com>
2025-07-08 12:51:40 -0300

    ✨ feat(footer): Update padding

--- src/View/footer.php
+++ src/View/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer p-2">
   <div class="has-text-centered">
     <p>Projeto de <strong><?=$_ENV['project_author'] ?></strong></p>
     <p><i>Feito em <?=$_ENV['project_since'] ?></i></p>
