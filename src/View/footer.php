5bb5f9d0dd85ad404bac99a3a4c7e9384929e38d
LyraMaker <gabriel.lyrat@gmail.com>
2025-07-08 12:49:31 -0300

    ✨ feat(footer): Simplify footer content structure

--- src/View/footer.php
+++ src/View/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <div class="content has-text-centered">
+  <div class="has-text-centered">
     <p>Projeto de <strong><?=$_ENV['project_author'] ?></strong></p>
     <p><i>Feito em <?=$_ENV['project_since'] ?></i></p>
   </div>
